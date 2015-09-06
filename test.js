66d2412a423f03a672dbfda9e62a5c89bcf95794
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-09-06 21:59:32 +0200

    Improve test coverage

--- test.js
+++ test.js
@@ -2,13 +2,21 @@
 var test = require('ava');
 var isClientError = require('./');
 
-test('should detect client error codes', function (t) {
+test('detects client error codes', function (t) {
 	t.true(isClientError(404));
 	t.true(isClientError(400));
 	t.true(isClientError(494));
-	t.true(!isClientError(308));
-	t.true(!isClientError(425));
-	t.true(!isClientError(441));
+	t.false(isClientError(308));
+	t.false(isClientError(425));
+	t.false(isClientError(441));
+
+	t.end();
+});
+
+test('throws when not passing a number', function (t) {
+	t.throws(function () {
+		isClientError({});
+	});
 
 	t.end();
 });
